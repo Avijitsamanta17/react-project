8530a054410419a6bf92daba6354fee267dd7283
Avijit Samanta <142030645+Avijitsamanta17@users.noreply.github.com>
2024-03-02 08:55:15 +0530

    Add files via upload

---
+++ client/src/components/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+export default function About() {
+  return (
+    <div className='about-h3'>
+     <h3>Project done by following students:</h3>
+     <div>
+        <ListGroup>
+            <ListGroupItem>Umme Fatema Mullick</ListGroupItem>
+            <ListGroupItem>Firoj Sardar</ListGroupItem>
+            <ListGroupItem>Avijit Samanta</ListGroupItem>
+            <ListGroupItem>Puja Kumari</ListGroupItem>
+            <ListGroupItem>Yamina Parvez</ListGroupItem>
+            <ListGroupItem>Swarnavo Mukherjee</ListGroupItem>
+            <ListGroupItem>Firoj Anjum</ListGroupItem>
+        </ListGroup>
+     </div>
+    </div>
+  )
+}
